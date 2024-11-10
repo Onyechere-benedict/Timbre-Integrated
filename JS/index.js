6381cbc152143d01d7911a6c2d18273a0a6eaa07
Noah-V <noahvikoo3@gmail.com>
2024-11-10 15:24:34 -0600

    Noah's Commit: Preloader

--- JS/index.js
+++ JS/index.js
@@ -1,80 +1,80 @@
 $(function () {
-    // SHOW SEARCH BAR
-    $(".search-button").click(function () {
-        $(".search-bar").addClass("active");
-    });
-    // HIDE SEARCH BAR
-    $(".search-bar .close-btn").click(function () {
-        $(".search-bar").removeClass("active");
-    });
+	// SHOW SEARCH BAR
+	$(".search-button").click(function () {
+		$(".search-bar").addClass("active");
+	});
+	// HIDE SEARCH BAR
+	$(".search-bar .close-btn").click(function () {
+		$(".search-bar").removeClass("active");
+	});
 
-    // HIDE/SHOW MOBILE MENU
-    $(".mobile-navbar .menu-open").click(function () {
-        $("body").addClass("menu-showing");
-    });
-    $(".mobile-navbar .menu-close").click(function () {
-        $("body").removeClass("menu-showing");
-    });
+	// HIDE/SHOW MOBILE MENU
+	$(".mobile-navbar .menu-open").click(function () {
+		$("body").addClass("menu-showing");
+	});
+	$(".mobile-navbar .menu-close").click(function () {
+		$("body").removeClass("menu-showing");
+	});
 
-    // HIDE/SHOW MOBILE MENU ITEMS
-    toggleMobileNav();
-    window.onresize = toggleMobileNav;
+	// HIDE/SHOW MOBILE MENU ITEMS
+	toggleMobileNav();
+	window.onresize = toggleMobileNav;
 
-    function toggleMobileNav() {
-        const mediaQuery = window.matchMedia("(max-width: 1199px)");
+	function toggleMobileNav() {
+		const mediaQuery = window.matchMedia("(max-width: 1199px)");
 
-        if (mediaQuery.matches) {
-            $("nav .submenu, nav .submenu-category").slideUp();
-            $("nav .menu-item .toggle-menu").click(function () {
-                $(this).siblings(".submenu").slideToggle();
-                $(this).toggleClass("rotate");
-            });
-            $("nav .submenu-item .toggle-menu").click(function () {
-                $(this).siblings(".submenu-category").slideToggle();
-                $(this).toggleClass("rotate");
-            });
-        } else $("nav .submenu, nav .submenu-category").slideDown("fast");
-    }
+		if (mediaQuery.matches) {
+			$("nav .submenu, nav .submenu-category").slideUp();
+			$("nav .menu-item .toggle-menu").click(function () {
+				$(this).siblings(".submenu").slideToggle();
+				$(this).toggleClass("rotate");
+			});
+			$("nav .submenu-item .toggle-menu").click(function () {
+				$(this).siblings(".submenu-category").slideToggle();
+				$(this).toggleClass("rotate");
+			});
+		} else $("nav .submenu, nav .submenu-category").slideDown("fast");
+	}
 
-    // TOGGLE TESTIMONIAL CLIENT REVIEWS ON HOME 3
+	// TOGGLE TESTIMONIAL CLIENT REVIEWS ON HOME 3
 
-    let currentReview = 0;
+	let currentReview = 0;
 
-    let reviewSlide = setInterval(() => {
-        nextReview2();
-    }, 3000);
+	let reviewSlide = setInterval(() => {
+		nextReview2();
+	}, 3000);
 
-    $(".client-reviews").hover(
-        () => {
-            // pause on mouse over
-            clearInterval(reviewSlide);
-        },
-        () => {
-            // resume on mouse out
-            reviewSlide = setInterval(() => {
-                nextReview2();
-            }, 3000);
-        }
-    );
+	$(".client-reviews").hover(
+		() => {
+			// pause on mouse over
+			clearInterval(reviewSlide);
+		},
+		() => {
+			// resume on mouse out
+			reviewSlide = setInterval(() => {
+				nextReview2();
+			}, 3000);
+		}
+	);
 
-    $(".nav-dots .dot-outer").click(function () {
-        nextReview1(parseInt($(this).attr("data-index")));
-    });
+	$(".nav-dots .dot-outer").click(function () {
+		nextReview1(parseInt($(this).attr("data-index")));
+	});
 
-    function nextReview1(index) {
-        $(".client-review").removeClass("active");
-        $(`.client-review:eq(${index})`).addClass("active");
+	function nextReview1(index) {
+		$(".client-review").removeClass("active");
+		$(`.client-review:eq(${index})`).addClass("active");
 
-        $(".nav-dots .dot-outer").removeClass("active");
-        $(`.nav-dots .dot-outer:eq(${index})`).addClass("active");
+		$(".nav-dots .dot-outer").removeClass("active");
+		$(`.nav-dots .dot-outer:eq(${index})`).addClass("active");
 
-        currentReview = index + 1;
-        if (currentReview === 3) currentReview = 0;
-    }
+		currentReview = index + 1;
+		if (currentReview === 3) currentReview = 0;
+	}
 
-    function nextReview2() {
-        nextReview1(currentReview);
-    }
+	function nextReview2() {
+		nextReview1(currentReview);
+	}
 });
 
 // const observeServiceElements = document.querySelectorAll(".col1");
@@ -98,34 +98,25 @@ $(function () {
 // observeServiceElements.forEach((el) => observer.observe(el));
 
 const observeElements = (selectors, threshold = 0, rootMargin = "0px 0px 0px 0px") => {
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                entry.target?.classList.toggle("observed", entry.isIntersecting);
-            });
-        },
-        { threshold, rootMargin }
-    );
-    selectors.forEach((selector) => {
-        const elements = document.querySelectorAll(selector);
-        elements.forEach((element) => observer.observe(element));
-        // document.querySelectorAll(selector).forEach(observer.observe.bind(observer));
-    });
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				entry.target?.classList.toggle("observed", entry.isIntersecting);
+			});
+		},
+		{ threshold, rootMargin }
+	);
+	selectors.forEach((selector) => {
+		const elements = document.querySelectorAll(selector);
+		elements.forEach((element) => observer.observe(element));
+		// document.querySelectorAll(selector).forEach(observer.observe.bind(observer));
+	});
 };
 
 observeElements(
-    [
-        ".col1",
-        ".col2 .col",
-        ".inner.centered header",
-        ".process-card",
-        ".call-to-action .inner .text",
-        ".blog-card",
-        ".blog-card-alt",
-        ".team-member-card",
-    ],
-    0,
-    "0px 0px 0px 3000px"
+	[".col1", ".col2 .col", ".inner.centered header", ".process-card", ".call-to-action .inner .text", ".blog-card", ".blog-card-alt", ".team-member-card"],
+	0,
+	"0px 0px 0px 3000px"
 );
 
 const popupToggler = document.querySelector(".toggle-popup");
@@ -133,11 +124,21 @@ const popupDialog = document.querySelector(".popup-overlay");
 const removePopupDialog = document.querySelector("#closePopup");
 
 const togglePopup = () => {
-    popupDialog.classList.toggle("active");
-    document.body.classList.toggle("no-scroll");
+	popupDialog.classList.toggle("active");
+	document.body.classList.toggle("no-scroll");
 };
 
 popupToggler.onclick = togglePopup;
 removePopupDialog.onclick = togglePopup;
 
 const signupButton = document.querySelector(".sign-up-btn");
+
+const loader = document.querySelector(".loader");
+
+const preloaderContainer = document.querySelector("#preloader");
+
+window.addEventListener("load", () => {
+	setTimeout(() => {
+		preloaderContainer.style.display = "none";
+	}, 4000);
+});
